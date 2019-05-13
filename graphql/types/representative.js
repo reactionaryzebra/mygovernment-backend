a5b42b3189c0e1c36fe650be2125742a271d174d
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-05-13 09:10:37 -0700

    Add representative type

---
+++ graphql/types/representative.js
@@ -0,0 +1,14 @@
+export default `
+type Representative {
+  office: String!
+  name: String!
+  party: String!
+  phones: [String!]!
+  urls: [String!]!
+  photoUrl: String!
+  channels: [Channel!]!
+  roles: [Committee!]
+  bills: [Bill!]
+  news: [Article]
+}
+`;
